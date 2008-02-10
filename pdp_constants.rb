e077619398027ab2d69e28c09069f42f464aeb49
sdague <sdague@fd7e7ab1-20b9-4e94-ab77-581fd4809956>
2008-02-10 20:29:51 +0000

    further sample code
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rtc/trunk@5 fd7e7ab1-20b9-4e94-ab77-581fd4809956

--- pdp_constants.rb
+++ pdp_constants.rb
@@ -46,16 +46,86 @@ module Proliphix
                                 1 => "Off",
                                 2 => "On",
                             })
-
-    ThermSetbackHeat = OID.new("thermSetbackHeat",
-                              "4.1.5",
-                              false)
     
-     
+    ThermSetbackHeat = OID.new("thermSetbackHeat",
+                               "4.1.5",
+                               false)
+    
+    ThermSetbackCool = OID.new("thermSetbackCoo",
+                               "4.1.6",
+                               false)
+    
+    # only on the NT150, and the math won't really work on this now anyway
+    ThermConfigHumidyCool = OID.new("thermConfigHumidityCool",
+                                    "4.2.22",
+                                    false)
+    
+    ThermSetbackStatus = OID.new("thermSetbackStatus",
+                                 "4.1.9",
+                                 false,
+                                 {
+                                     1 => "Normal",
+                                     2 => "Hold",
+                                     3 => "Override"
+                                 })
 
+    ThermCurrentPeriod = OID.new("thermCurrentPeriod",
+                                 "4.1.10",
+                                 true, # false, but the spec says writes are ignored
+                                 {
+                                     1 => "Morn",
+                                     2 => "Day",
+                                     3 => "Eve",
+                                     4 => "Night",
+                                 })
 
-    ThermAvgTemp = "4.1.13"
-    ThermSensorTemp1 = "4.3.2.1"
-    ThermSensorTemp2 = "4.3.2.2"
-    ThermSensorTemp3 = "4.3.2.3"
+    ThermActivePeriod = OID.new("thermActivePeriod",
+                                "4.1.12",
+                                true,
+                                { 
+                                    1 => "Morn",
+                                    2 => "Day",
+                                    3 => "Eve",
+                                    4 => "Night",
+                                    5 => "Hold",
+                                    6 => "Override"
+                                })
+
+    ThermCurrentClass = OID.new("thermCurrenClass",
+                                "4.1.11",
+                                true,
+                                {
+                                    1 => "In",
+                                    2 => "Out",
+                                    3 => "Away"
+                                })
+    
+    ThermAverageTemp = OID.new("thermAverageTemp",
+                               "4.1.13",
+                               true)
+    
+    ThermHeat1Usage = OID.new("thermHeat1Usage",
+                              "4.5.1",
+                              true) # it says false, but that's just too hard for me to wrap my brain around
+
+    ThermHeat2Usage = OID.new("thermHeat2Usage",
+                              "4.5.2",
+                              true) # it says false, but that's just too hard for me to wrap my brain around
+
+    ThermCool1Usage = OID.new("thermCool1Usage",
+                              "4.5.3",
+                              true) # it says false, but that's just too hard for me to wrap my brain around
+
+    ThermCool2Usage = OID.new("thermCool1Usage",
+                              "4.5.4",
+                              true) # it says false, but that's just too hard for me to wrap my brain around
+    
+    ThermFanUsage = OID.new("thermFanUsage",
+                            "4.5.5",
+                            true)
+
+    ThermLastUsageReset = OID.new("thermLastUsageReset",
+                                  "4.5.6",
+                                  false) # we're going to have to be tricky about this one
+    
 end
--- thermostat.rb
+++ thermostat.rb
@@ -1,15 +1,30 @@
 require 'net/http'
 require 'uri'
 require 'open-uri'
+require 'pdp_constants'
 
 class Thermostat
     def initialize(ip, user, passwd)
         @ip = ip
         @user = user
         @passwd = passwd
+        @fetched = {}
     end
 
     def url
         return "http://#{@ip}"
+    end
+
+    # Because of the timing issues with the thermostat we want to get all the data we can in one packet.
+    def fetch_data(array)
+        query = ""
+        for item in array
+            if query != ""
+                query += "&"
+            end
+            query += "OID#{item.oid}="
+        end
+        query = url + "/get?" + query
+        puts query
     end
 end
