3bda25b8697da8696a69024fe9dc3096a9d390e8
Sean Dague <sean@dague.net>
2010-03-09 18:33:55 -0500

    this is less generic, but I'm not going to go for generic at this point

--- lib/thermostat.rb
+++ lib/thermostat.rb
@@ -1,23 +1,51 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-require "pdp/thermostat"
 
 # order here is important
 
 class Thermostat
-    VERISON = '1.0.0'
-    
-    
-    def initialize()
+    require 'pdp/oid'
+    require 'pdp/constants'
+    require 'pdp/network'
+            
+    def initialize(ip, user, passwd)
+        @network = Proliphix::Network.new(ip, user, passwd)
     end
-   
+    
+    def [](reading)
+        return @network.get(reading)
+    end
+    
+    def []=(reading, value)
+        return @network.set(reading, value)
+    end
+    
+    def mode
+        self[ThermHvacMode]
+    end
+    
+    def target_heat
+        case mode
+        when "Heat" then return self[ThermSetbackHeat]
+        when "Auto" then return self[ThermSetbackHeat]
+        end
+        return nil
+    end
+    
+    def target_cool
+        case mode
+        when "Cool" then return self[ThermSetbackCool]
+        when "Auto" then return self[ThermSetbackCool]
+        end
+        return nil
+    end
+
+        
     def temp
-        
+        self[ThermAverageTemp]
     end
     
-    def fan
-        
-    end
 end
+
 
