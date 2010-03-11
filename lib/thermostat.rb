bef2bfe856a301eb6ab0f2e71542e7223406f115
Sean Dague <sean@dague.net>
2010-03-11 08:00:40 -0500

    updated to 1.1.1 because the examples were wrong previously

--- lib/thermostat.rb
+++ lib/thermostat.rb
@@ -4,7 +4,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 # This is the main class for controlling the Proliphix Thermostat.
 # You must first initialize it with the control point credentials
 class Thermostat
-    VERSION = '1.1.0'
+    VERSION = '1.1.1'
         
     require 'pdp/oid'
     require 'pdp/constants'
